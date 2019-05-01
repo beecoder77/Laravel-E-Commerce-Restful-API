656b3147b39b3d4c6223458657792f0824ea52b0
Muhammad Salman Abid <ealahherp@gmail.com>
2019-05-01 14:28:45 +0700

    Seeding Product dan Review nya

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'nama' => $faker->name,
+        'detail' => $faker->paragraph,
+        'harga' => $faker->numberBetween(100000,10000000),
+        'stok'=> $faker->randomDigit,
+        'diskon' => $faker->numberBetween(2,50)
     ];
 });
