6a3b3d3de119d90e9b4117600d9a59f8a12a14e2
Muhammad Salman Abid <ealahherp@gmail.com>
2019-05-01 14:49:42 +0700

    edit Product Resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends Resource
             'nama' => $this->nama,
             'deskripsi' => $this->detail,
             'harga' => $this->harga,
-            'stok' => $this->stok,
+            'stok' => $this->stok == 0 ? 'Stok Habis' : $this->stok,
             'diskon' =>$this->diskon,
+            'totalHarga' => round(( 1 - ($this->diskon/100)) * $this->harga,0),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'Belum ada Rating',
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
