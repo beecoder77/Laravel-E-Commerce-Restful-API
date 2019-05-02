adfc8ff02869a5667b5186c3f9e54097df57bb38
Muhammad Salman Abid <ealahherp@gmail.com>
2019-05-03 04:43:33 +0700

    complete projek

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends Resource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'customer' => $this->customer,
             'review' => $this->review,
             'star' => $this->star
